8db8d1da9842b6f3749e989f8175f7f6cb3908c6
ashwin2003 <ashwinjagarwal123@gmail.com>
2023-01-07 04:05:03 +0530

    env variable

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
 
-const server = app.listen(4000, () => {
-  console.log(`Server is running on ${4000}`);
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server is running on ${process.env.PORT}`);
 });
